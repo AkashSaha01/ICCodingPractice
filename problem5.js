3343c254185b8b492bdbed6495cd16955322d013
AkashSaha01 <akashsaha661@gmail.com>
2025-06-04 22:58:17 +0600

    problem 5 in multiple way

--- problem5.js
+++ problem5.js
@@ -1,5 +1,15 @@
 var mergedArray = (a, b) => {
   var combined = [...a, ...b];
-  console.log(combined.sort((x, y) => x - y));
+  console.log(
+    "First try for Sorted merged array : " + combined.sort((x, y) => x - y)
+  );
+};
+
+var mergedArrayAlternative = (a, b) => {
+  var combined = [...a];
+  // console.log(combined);
+  combined.push(...b);
+  console.log("Another Try for sorting : " + combined.sort((x, y) => x - y));
 };
 mergedArray([2, 8, 6, 11, 3], [9, 5, 1, 2]);
+mergedArrayAlternative([2, 8, 6, 11, 3], [9, 5, 1, 2]);
